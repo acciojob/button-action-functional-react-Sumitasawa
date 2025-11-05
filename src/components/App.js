aac6ad63f9057768f856334cbda5e1dcef8e5f48
Sumitasawa <sumitasawa65@gmail.com>
2025-11-05 11:13:01 +0530

    Inital Commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showpara,setshowpara]=useState(false);
+
+
+  const handleclick=()=>{
+    setshowpara(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+     <button id="click" onClick={handleclick}>Click Me</button>
+
+
+     {showpara&&(<p  id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>)}
     </div>
   );
 }
